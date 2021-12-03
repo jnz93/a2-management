52c8968b450aa11242b367d079f1be3fb037963b
Joanez Andrades <suportedeathofday@gmail.com>
2021-12-03 19:32:23 -0300

    first commit

---
+++ includes/class-a2-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       unitycode.tech
+ * @since      1.0.0
+ *
+ * @package    A2
+ * @subpackage A2/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    A2
+ * @subpackage A2/includes
+ * @author     jnz93 <box@unitycode.tech>
+ */
+class A2_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
