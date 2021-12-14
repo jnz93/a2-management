dc6e3fbf928821b64731addf7637352262cbd694
Joanez Andrades <suportedeathofday@gmail.com>
2021-12-14 18:26:51 -0300

    Adição da classe A2_Register()

---
+++ includes/class-a2-register.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The file that defines the registration class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @package    A2
+ * @subpackage A2/includes
+ * @since      1.0.0
+ * @author     jnz93 <box@unitycode.tech>
+ */
+class A2_Register{
+
+    /**
+     * Update Roles Version
+     */
+    private $updateRolesVersion;
+
+    /**
+     *  Remove roles version
+     */
+    private $removeRolesVersion;
+
+}
