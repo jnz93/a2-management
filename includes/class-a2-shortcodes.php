0874bd1396b235316d4766478934942a171c2dcb
Joanez Andrades <suportedeathofday@gmail.com>
2021-12-18 16:05:33 -0300

    Adicionado shortcode registerForm

--- includes/class-a2-shortcodes.php
+++ includes/class-a2-shortcodes.php
@@ -26,5 +26,29 @@ class A2_Shortcodes{
     public function __construct()
     {
         $this->register = new A2_Register();
+
+        /** Formulário de cadastro */
+        add_shortcode( 'registerForm', [ $this, 'registerForm'] );
+    }
+
+    /**
+     * Shortcode: Formulário de cadastro de usuários
+     * 
+     * Após a seleção do tipo de perfil que o usuário deseja ser um formulário será mostrado.
+     * Este Formulário é chamado pelo método processForm() da classe A2_Register();
+     */
+    public function registerForm( $atts )
+    {
+        $a = shortcode_atts( 
+            [
+                'title' => 'Formulário de Cadastro'
+			], 
+            $atts
+        );
+
+        ob_start();
+        $this->register->proccessForm();
+        
+        return ob_get_clean();
     }
 }
