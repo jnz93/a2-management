733757101c7dc8b7b779d9897568747774fa4be8
Joanez Andrades <suportedeathofday@gmail.com>
2021-12-21 15:08:25 -0300

    Adição do shortcode loginForm a classe A2_Shortcodes()

--- includes/class-a2-shortcodes.php
+++ includes/class-a2-shortcodes.php
@@ -29,6 +29,9 @@ class A2_Shortcodes{
 
         /** Formulário de cadastro */
         add_shortcode( 'registerForm', [ $this, 'registerForm'] );
+
+        /** Formulário de login */
+        add_shortcode( 'loginForm', [ $this, 'loginForm'] );
     }
 
     /**
@@ -49,6 +52,41 @@ class A2_Shortcodes{
         ob_start();
         $this->register->proccessForm();
         
+        return ob_get_clean();
+    }
+
+    /**
+     * Shortcode: Formulário de login
+     * 
+     * Após o login, o tipo de usuário é identificado e redirecionado para o painel correspondente.
+     * Usuários logados devem ser redirecionados para o painel correspondente
+     */
+    public function loginForm( $atts )
+    {
+        $a = shortcode_atts( 
+            [
+                'title' => 'Formulário de Cadastro'
+			], 
+            $atts
+        );
+        
+        $login = ( isset( $_GET['login'] ) ) ? $_GET['login'] : 0;
+        $args = array(
+            'redirect'          => home_url('/painel'),
+        );
+
+        ob_start();
+
+        if( $login === 'failed' ){
+            echo '<p class="login-message"> <strong class="">'. __('ERRO: ') .'</strong>'. __('Nome ou senha inválidos.') .'</p>';
+        } elseif( $login === 'empty' ){
+            echo '<p class="login-message"> <strong class="">'. __('ERRO: ') .'</strong>'. __('Nome ou senha estão vazios.') .'</p>';
+        } elseif( $login === 'false' ){
+            echo '<p class="login-message"> <strong class="">'. __('ERRO: ') .'</strong>'. __('Você está desconectado.') .'</p>';
+        }
+
+        wp_login_form( $args );
+
         return ob_get_clean();
     }
 }
