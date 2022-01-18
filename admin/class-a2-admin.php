3c56974c4a5043a694363cd88012ea6e20199c38
Joanez Andrades <suportedeathofday@gmail.com>
2022-01-18 19:29:55 -0300

    Registro do custom post type a2_escorts

--- admin/class-a2-admin.php
+++ admin/class-a2-admin.php
@@ -67,6 +67,11 @@ class A2_Admin {
 		 * Adição do menu na dashboard admin
 		 */
 		add_action( 'admin_menu', array( $this, 'addMenuPageWpDashboard' ) );
+
+		/**
+		 * Custom post types
+		 */
+		add_action( 'init', [ $this, 'registerCustomPostTypes' ] );
 	}
 
 	/**
@@ -139,5 +144,57 @@ class A2_Admin {
 	public function cbPluginPage()
 	{
 		require_once plugin_dir_path( __FILE__ ) . 'partials/a2-admin-display.php';
+	}
+
+	/** 
+	 * Custom post type
+	 * Criação de CPT para acompanhantes
+	 * 
+	 */
+	public function registerCustomPostTypes()
+	{
+		$labels = array(
+			'name'                  => _x( 'Acompanhantes', 'Post type general name', 'textdomain' ),
+			'singular_name'         => _x( 'Acompanhante', 'Post type singular name', 'textdomain' ),
+			'menu_name'             => _x( 'Acompanhantes', 'Admin Menu text', 'textdomain' ),
+			'name_admin_bar'        => _x( 'Acompanhantes', 'Add New on Toolbar', 'textdomain' ),
+			'add_new'               => __( 'Adicionar nova(o)', 'textdomain' ),
+			'add_new_item'          => __( 'Adicionar nova(o)', 'textdomain' ),
+			'new_item'              => __( 'Nova(o) Acompanhante', 'textdomain' ),
+			'edit_item'             => __( 'Editar Acompanhante', 'textdomain' ),
+			'view_item'             => __( 'Visualizar Acompanhante', 'textdomain' ),
+			'all_items'             => __( 'Todas Acompanhantes', 'textdomain' ),
+			'search_items'          => __( 'Procurar Acompanhante', 'textdomain' ),
+			'parent_item_colon'     => __( 'Parent Acompanhante:', 'textdomain' ),
+			'not_found'             => __( 'Nenhum Acompanhante encontrada(o).', 'textdomain' ),
+			'not_found_in_trash'    => __( 'Nenhum Acompanhante encontrada(o) na lixeira.', 'textdomain' ),
+			'featured_image'        => _x( 'Foto Perfil Acompanhante', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'set_featured_image'    => _x( 'Definir Foto de Perfil', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'remove_featured_image' => _x( 'Remover Foto de Perfil', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'use_featured_image'    => _x( 'Usar como foto de perfil', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'archives'              => _x( 'Arquivos de acompanhantes', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+			'insert_into_item'      => _x( 'Inserir Acompanhante', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+			'uploaded_to_this_item' => _x( 'Carregado para este acompanhante', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+			'filter_items_list'     => _x( 'Filtrar lista de Acompanhantes', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+			'items_list_navigation' => _x( 'Navegação da lista de Acompanhantes', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+			'items_list'            => _x( 'Lista de acompanhantes', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'acompanhante' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
+		);
+
+		register_post_type( 'a2_escorts', $args );
 	}
 }
