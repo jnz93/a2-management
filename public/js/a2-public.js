e431a9bd776fde863fc8f5665151981094b4108a
Joanez Andrades <suportedeathofday@gmail.com>
2022-02-11 03:00:04 -0300

    Adição da função profileActivateMasks() para ativar as mascáras na edição do perfil

--- public/js/a2-public.js
+++ public/js/a2-public.js
@@ -30,3 +30,19 @@
 	 */
 
 })( jQuery );
+
+function profileActivateMasks()
+{
+	jQuery('#_profile_whatsapp').mask('(00) 00000-0000');
+	jQuery('#_profile_height').mask('0,00');
+	jQuery('#_profile_weight').mask('000');
+	jQuery('#_profile_tits_size').mask('000');
+	jQuery('#_profile_bust_size').mask('000');
+	jQuery('#_profile_waist_size').mask('000');
+	jQuery('#_profile_cep').mask('00000-000');
+	jQuery('#_profile_cache_quickie').mask('000.000.000.000.000,00', {reverse: true});
+	jQuery('#_profile_cache_half_an_hour').mask('000.000.000.000.000,00', {reverse: true});
+	jQuery('#_profile_cache_hour').mask('000.000.000.000.000,00', {reverse: true});
+	jQuery('#_profile_cache_overnight_stay').mask('000.000.000.000.000,00', {reverse: true});
+	jQuery('#_profile_cache_promotion').mask('000.000.000.000.000,00', {reverse: true});
+}
