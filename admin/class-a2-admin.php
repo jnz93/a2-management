6049f0cc301eb7e9545a75dc23126f56e3f3c4f5
Joanez Andrades <suportedeathofday@gmail.com>
2022-03-29 14:51:41 -0300

    Registrando novo Custom post type a2_advertisement

--- admin/class-a2-admin.php
+++ admin/class-a2-admin.php
@@ -190,7 +190,6 @@ class A2_Admin {
 			'items_list_navigation' => _x( 'Navegação da lista de Acompanhantes', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
 			'items_list'            => _x( 'Lista de acompanhantes', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
 		);
-
 		$args = array(
 			'labels'             => $labels,
 			'public'             => true,
@@ -205,8 +204,53 @@ class A2_Admin {
 			'menu_position'      => null,
 			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
 		);
-
 		register_post_type( 'a2_escort', $args );
+		
+		unset($labels);
+		unset($args);
+
+		$labels = array(
+			'name'                  => _x( 'Anúncios', 'Post type general name', 'textdomain' ),
+			'singular_name'         => _x( 'Anúncio', 'Post type singular name', 'textdomain' ),
+			'menu_name'             => _x( 'Anúncios', 'Admin Menu text', 'textdomain' ),
+			'name_admin_bar'        => _x( 'Anúncios', 'Add New on Toolbar', 'textdomain' ),
+			'add_new'               => __( 'Adicionar nova(o)', 'textdomain' ),
+			'add_new_item'          => __( 'Adicionar nova(o)', 'textdomain' ),
+			'new_item'              => __( 'Nova(o) Anúncio', 'textdomain' ),
+			'edit_item'             => __( 'Editar Anúncio', 'textdomain' ),
+			'view_item'             => __( 'Visualizar Anúncio', 'textdomain' ),
+			'all_items'             => __( 'Todas Anúncios', 'textdomain' ),
+			'search_items'          => __( 'Procurar Anúncio', 'textdomain' ),
+			'parent_item_colon'     => __( 'Parent Anúncio:', 'textdomain' ),
+			'not_found'             => __( 'Nenhum Anúncio encontrada(o).', 'textdomain' ),
+			'not_found_in_trash'    => __( 'Nenhum Anúncio encontrada(o) na lixeira.', 'textdomain' ),
+			'featured_image'        => _x( 'Foto Perfil Anúncio', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'set_featured_image'    => _x( 'Definir Capa do anúncio', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'remove_featured_image' => _x( 'Remover Capa do anúncio', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'use_featured_image'    => _x( 'Usar como Capa do anúncio', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+			'archives'              => _x( 'Arquivos de Anúncios', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+			'insert_into_item'      => _x( 'Inserir Anúncio', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+			'uploaded_to_this_item' => _x( 'Carregado para este Anúncio', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+			'filter_items_list'     => _x( 'Filtrar lista de Anúncios', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+			'items_list_navigation' => _x( 'Navegação da lista de Anúncios', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+			'items_list'            => _x( 'Lista de Anúncios', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+		);
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'anuncio' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
+		);
+		register_post_type( 'a2_advertisement', $args );
+		
 	}
 
 	/**
