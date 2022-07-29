e20881fb0433ce37dedafdfc925359a689c7885a
Joanez Andrades <suportedeathofday@gmail.com>
2022-07-29 00:37:48 -0300

    Add shortcode [activeAdvInfo]

---
+++ public/partials/dashboard/tpl-advCardInfo.php
@@ -0,0 +1,18 @@
+<div class="advCardInfo mb-4">
+    <span class="advCardInfo__title mb-2 d-block"><?php echo $a['title']; ?></span>
+
+    <div class="card advCardInfo__container advCardInfo--diamond">
+        <div class="advCardInfo__top">
+            <div class="advCardInfo__icon mb-2">
+                <?php echo $advIcon; ?>
+            </div>
+            <span class="advCardInfo__text text-uppercase fw-bold" style="color: <?php echo $colorOne; ?>;"><?php _e( $advType, 'textdomain' ); ?></span>
+        </div>
+
+        <div class="advCardInfo__bottom">
+            <span class="advCardInfo__text" style="color: <?php echo $colorTwo; ?>;"><?php _e( 'Válido até:', 'textdomain' ); ?></span>
+            <h4 class="mt-2 mb-2" style="color: <?php echo $colorOne; ?>;"><?php echo $date ?></h4>
+            <span class="advCardInfo__text" style="color: <?php echo $colorTwo; ?>;"><?php _e( 'às ' . $hour, 'textdomain'); ?></span>
+        </div>
+    </div>
+</div>
